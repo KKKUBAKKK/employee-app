0086d484708cb6e1e9b33d07827cd11a7ac7402c
KKKUBAKKK <kuba.kindracki@gmail.com>
2025-01-09 18:54:57 +0100

    magically started working

--- employee-app/obj/Debug/net8.0/employee-app.AssemblyInfo.cs
+++ employee-app/obj/Debug/net8.0/employee-app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("employee-app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0500f82e49ec2098c8ce65aca120111d1d67b3a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99c4559b55893486f940be559cd497de9399f57c")]
 [assembly: System.Reflection.AssemblyProductAttribute("employee-app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("employee-app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
