a89f33d6d69fee8b3af791ed6c6065de9cf36fd8
Janek <Janek@JANEK>
2025-01-10 22:14:01 +0100

    customer button

--- employee-app/Models/LoginModel.cs
+++ employee-app/Models/LoginModel.cs
@@ -2,6 +2,6 @@ namespace employee_app.Models;
 
 public class LoginModel
 {
-    public string Username { get; set; }
-    public string Password { get; set; }
+    public string? Username { get; set; }
+    public string? Password { get; set; }
 }
