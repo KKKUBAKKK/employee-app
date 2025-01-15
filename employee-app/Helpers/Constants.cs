4528e93dac0a892752f7426d8ac0d1ec949eaae7
KKKUBAKKK <kuba.kindracki@gmail.com>
2025-01-15 18:02:17 +0100

    fixed displaying username and the link to user rental

--- employee-app/Helpers/Constants.cs
+++ employee-app/Helpers/Constants.cs
@@ -4,5 +4,5 @@ public static class Constants
 {
     public const string RentalName = "JEJ Car Rental";
 
-    public const string CustomerWebSide = "https://usosweb.usos.pw.edu.pl/kontroler.php?_action=news/default";
+    public const string CustomerWebSide = "http://molczane.github.io";
 }
