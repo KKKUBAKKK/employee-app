092244fc2810ed55a495b407b31cd85bee2943b8
KKKUBAKKK <kuba.kindracki@gmail.com>
2025-01-16 00:52:27 +0100

    Fixed the url

--- employee-app/Program.cs
+++ employee-app/Program.cs
@@ -11,8 +11,13 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+var baseAddress = builder.HostEnvironment.IsDevelopment() 
+    ? "http://localhost:5001" 
+    : "https://car-rental-api-chezbchwebfggwcd.canadacentral-01.azurewebsites.net";
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) });
 // builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://car-rental-api-chezbchwebfggwcd.canadacentral-01.azurewebsites.net") });
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5001") });
+// builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5001") });
 
 builder.Services.AddScoped<ICarService, CarService>();
 builder.Services.AddScoped<IRentalService, RentalService>();
