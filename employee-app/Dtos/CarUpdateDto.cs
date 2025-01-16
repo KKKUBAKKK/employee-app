7c73b11147db85f237058bed101709f23d3a95e6
KKKUBAKKK <kuba.kindracki@gmail.com>
2025-01-16 11:47:58 +0100

    Improved vehicles page, added updating and addign vehicles.

--- employee-app/Dtos/CarUpdateDto.cs
+++ employee-app/Dtos/CarUpdateDto.cs
@@ -1,7 +1,10 @@
+using employeeapp.Helpers;
+
 namespace employeeapp.Dtos;
 
 public class CarUpdateDto
 {
     public string Location { get; set; } = "Plac Politechniki, Warszawa";
     public bool IsAvailable { get; set; } = true;
+    public CarType Type { get; set; } = CarType.economy;
 }
